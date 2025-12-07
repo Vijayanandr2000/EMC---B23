3d23134e34a9a3531306221ac89774faa287a167
Vijayanandr2000 <vijayanandr2000@gmail.com>
2025-12-07 11:47:54 +0530

    class 4 code added

---
+++ class - 4/todo/script.js
@@ -0,0 +1,29 @@
+// console.log("Script loaded");
+
+// // document.addEventListener("DOMContentLoaded", () => {
+// //   // const button = document.getElementsByTagName("button")[0];
+// //   const button = document.querySelector("button");
+
+// //   console.log("button", button);
+// // });
+
+// const button = document.querySelector("button");
+// const input = document.querySelector("input"); // = ""
+// const p = document.querySelector("p");
+
+// function print() {
+//   console.log("button clicked");
+//   //   p.textContent = "<b>Anand</b>";
+//   //   p.innerText = "<b>Anand</b>";
+//   p.innerHTML = "<b>Anand</b>";
+//   console.log("Paragraph value: ", p.textContent);
+
+//   let inputValue = input.value;
+
+//   console.log("Input Value: ", inputValue);
+// }
+
+// // button.addEventListener("click", () => {
+// //   print();
+// //   //   console.log("Input Value: ", input);
+// // });
